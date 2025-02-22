8246a08b85b9fc7e18be26560a6349a0b15f6d30
taewoongheo <chamjoeun0111@gmail.com>
2025-02-22 17:37:46 +0900

    chore: 테스트를 위한 jest + react-native-testing-library 환경 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'eslint:recommended', 'plugin:jest/recommended'],
+  plugins: ['jest'],
   rules: {
     'react-hooks/exhaustive-deps': 'off',
   },
+  globals: {
+    'jest/globals': true,
+  },
+  overrides: [
+    {
+      // Test files only
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
 };
