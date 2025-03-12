b2ede4586590b5a5ae4e11a39e4761e2205f6252
taewoongheo <chamjoeun0111@gmail.com>
2025-03-12 20:38:35 +0900

    fix: 온도초과 알람 이후 바로 온도미만 알람이 뜨지 않는 문제 수정
    알람이 울리는 상태만 사용했더니, 초과 시 알람이 이전에 울렸기 때문에 미만이 되자마자 바로 알람이 울리지 않게 됨. 따라서 온도변화에 따른 세 가지 상태를 만들어 따로 처리함

--- src/components/SensorData/DataCardGrid/SensorDataAlert/index.jsx
+++ src/components/SensorData/DataCardGrid/SensorDataAlert/index.jsx
@@ -1,46 +1,55 @@
 import React, {useEffect, useState} from 'react';
 import {statusAlert} from '../../../../utils/alerts';
 import {ALERT_MESSAGES} from '../../../../constants/messages';
+import {TEMPERATURE_STATUS} from '../../../../constants/temperatureStatus';
 
 function SensorDataAlert(props) {
   const {sensorData, threshold, children} = props;
 
-  const [isAlerted, setIsAlerted] = useState(false);
+  const [temperatureStatus, setTemperatureStatus] = useState(
+    TEMPERATURE_STATUS.NORMAL,
+  );
 
   useEffect(() => {
     const temperature = sensorData.dht11.temperature;
     const temperatureTarget = threshold.temperature;
     const temperatureRange = threshold.tempRange;
-    if (temperature < temperatureTarget - temperatureRange) {
-      if (!isAlerted) {
-        setIsAlerted(true);
-        statusAlert(
-          ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.LOW,
-          ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.LOW_TEMPERATURE_MESSAGE(
-            temperature,
-            temperatureTarget,
-            temperatureRange,
-          ),
-          ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.CONFIRM,
-        );
-      }
-    } else if (temperature > temperatureTarget + temperatureRange) {
-      if (!isAlerted) {
-        setIsAlerted(true);
-        statusAlert(
-          ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.HIGH,
-          ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.HIGH_TEMPERATURE_MESSAGE(
-            temperature,
-            temperatureTarget,
-            temperatureRange,
-          ),
-          ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.CONFIRM,
-        );
-      }
+    if (
+      temperature < temperatureTarget - temperatureRange &&
+      temperatureStatus !== TEMPERATURE_STATUS.HIGH
+    ) {
+      setTemperatureStatus(TEMPERATURE_STATUS.HIGH);
+      statusAlert(
+        ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.LOW,
+        ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.LOW_TEMPERATURE_MESSAGE(
+          temperature,
+          temperatureTarget,
+          temperatureRange,
+        ),
+        ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.CONFIRM,
+      );
+    } else if (
+      temperature > temperatureTarget + temperatureRange &&
+      temperatureStatus !== TEMPERATURE_STATUS.LOW
+    ) {
+      setTemperatureStatus(TEMPERATURE_STATUS.LOW);
+      statusAlert(
+        ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.HIGH,
+        ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.HIGH_TEMPERATURE_MESSAGE(
+          temperature,
+          temperatureTarget,
+          temperatureRange,
+        ),
+        ALERT_MESSAGES.STATUS_CHANGE.TEMPERATURE.CONFIRM,
+      );
     } else {
-      setIsAlerted(false);
+      setTemperatureStatus(TEMPERATURE_STATUS.NORMAL);
     }
-  }, [sensorData, threshold]);
+  }, [
+    sensorData.dht11.temperature,
+    threshold.temperature,
+    threshold.tempRange,
+  ]);
 
   return <>{children}</>;
 }
---
+++ src/constants/temperatureStatus.js
@@ -0,0 +1,5 @@
+export const TEMPERATURE_STATUS = {
+  HIGH: 1,
+  LOW: -1,
+  NORMAL: 0,
+};
