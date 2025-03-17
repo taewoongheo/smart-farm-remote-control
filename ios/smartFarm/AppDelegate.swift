593f628038977f12032788087fecfea4f955fd53
taewoongheo <chamjoeun0111@gmail.com>
2025-03-17 21:20:41 +0900

    chore: react native와 firebase RTDB 연결을 위한 환경설정

--- ios/smartFarm/AppDelegate.swift
+++ ios/smartFarm/AppDelegate.swift
@@ -2,10 +2,14 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import Firebase
 
 @main
 class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    
+    FirebaseApp.configure()
+    
     self.moduleName = "smartFarm"
     self.dependencyProvider = RCTAppDependencyProvider()
 
