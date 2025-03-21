b82b3f93a55c88a0ab65ab41e103e9d63ef1d0ff
taewoongheo <chamjoeun0111@gmail.com>
2025-03-21 20:38:58 +0900

    feat: 작물별 생육 정보 fetch API 기능 추가

--- src/pages/CropInfo/CropInfo.jsx
+++ src/pages/CropInfo/CropInfo.jsx
@@ -1,13 +1,42 @@
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
+import {URL} from '../../../env';
 
 function CropInfo() {
   const navigation = useNavigation();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchUrl = async url => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUrl(URL);
+  }, []);
+
+  if (loading) {
+    return (
+      <View>
+        <Text>로딩중.....</Text>
+      </View>
+    );
+  }
 
   return (
     <View>
       <Text>Crop info</Text>
+
       <TouchableOpacity onPress={() => navigation.goBack('Main')}>
         <Text>main으로 이동</Text>
       </TouchableOpacity>
